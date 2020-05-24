ce803800cb8bd83beadd7e3c3ae2b0d499f92616
hgminh <hgminh95@gmail.com>
2020-05-24 13:45:59 +0800

    Various CSS update

--- gui/static/base.js
+++ gui/static/base.js
@@ -1,1 +1,19 @@
 let $ = id => document.getElementById(id);
+
+let g_toasting = false;
+
+function toast(msg) {
+  if (!g_toasting) {  /* handle multiple toast called, probably not perfect */
+    g_toasting = true;
+
+    let x = $("toast");
+    x.innerHTML = msg;
+    x.className = "show";
+    setTimeout(function() {
+      x.className = x.className.replace("show", "");
+      g_toasting = false;
+    }, 3000);
+  } else {
+    setTimeout(() => toast(msg), 1000);
+  }
+}
